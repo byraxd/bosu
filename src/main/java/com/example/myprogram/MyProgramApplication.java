b2ea89d9c4234adf6dfd7ba4dfc6cbfc81369287
Burlaka Olexander <byraxd@gmail.com>
2021-07-18 13:26:44 +0300

    init commit

---
+++ src/main/java/com/example/myprogram/MyProgramApplication.java
@@ -0,0 +1,13 @@
+package com.example.myprogram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyProgramApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyProgramApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/myprogram/MyProgramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myprogram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyProgramApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

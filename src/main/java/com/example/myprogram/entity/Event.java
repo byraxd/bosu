fda743912e940dfcaaef4dcdf76f0812dee9c7ef
Burlaka Olexander <byraxd@gmail.com>
2021-08-05 18:15:38 +0300

    fix id in Event entity

--- src/main/java/com/example/myprogram/entity/Event.java
+++ src/main/java/com/example/myprogram/entity/Event.java
@@ -1,13 +1,17 @@
 package com.example.myprogram.entity;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Event {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
     private String display_html;
     private Integer beatmap_id;
-    @Id
     private Integer beatmapset_id;
     private String date;
     private Integer epicfactor;
